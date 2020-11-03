b8f34a245b1c5b1705968fa8d46f5f6cfe59cc00
PiKouri <hok@etud.insa-toulouse.fr>
2020-11-03 16:48:19 +0100

    clone_nodes (no test)

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-ocamlc -c graph.mli graph.ml gfile.mli gfile.ml
-ocamlc -o ftest graph.cmo gfile.cmo ftest.ml
+#ocamlc -c graph.mli graph.ml gfile.mli gfile.ml
+#ocamlc -o ftest graph.cmo gfile.cmo ftest.ml
+rm -f *.cmi *.cmo ftest
+ocamlbuild ftest.byte
---
+++ tools.ml
@@ -0,0 +1,7 @@
+(* Yes, we have to repeat open Graph. *)
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph
+let gmap gr f = assert false
+let add_arc = assert false
