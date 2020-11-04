eaaa7be4b713a76aa3a06fbe83e35f8ec75417b3
PiKouri <hok@etud.insa-toulouse.fr>
2020-11-04 11:32:36 +0100

    tools.ml complet (sans test)

--- tools.ml
+++ tools.ml
@@ -2,6 +2,16 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
+
+(*returns a new graph having the same nodes than gr, but no arc.*)
 let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph
-let gmap gr f = assert false
-let add_arc = assert false
+
+(*maps all arcs of gr by function f.*)
+let gmap gr f = let new_graph = clone_nodes gr 
+				in let map graph id1 id2 arc f = new_arc graph id1 id2 (f arc)
+				in e_fold gr map new_graph
+
+(*adds n to the value of the arc between id1 and id2. If the arc does not exist, it is created.*)
+let add_arc gr id1 id2 n = match (find_arc gr id1 id2) with
+	| None -> new_arc gr id1 id2 n
+	| Some x -> new_arc gr id1 id2 (x+n)
